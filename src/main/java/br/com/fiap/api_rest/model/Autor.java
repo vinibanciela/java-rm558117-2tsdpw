68c896fb0935cd80a588982585cc18ec0e780d06
vinibanciela <vinibanciela@gmail.com>
2025-03-18 22:43:45 -0300

    add a classe autor em model

---
+++ src/main/java/br/com/fiap/api_rest/model/Autor.java
@@ -0,0 +1,40 @@
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @OneToMany
+    private List<Livro> livros;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+}
